75021a0e0e5651d91ee012d1d4e29945a67cac00
jnetto87 <josenetto102@gmail.com>
2024-01-02 21:30:28 -0400

    commit previsao

--- scripts.js
+++ scripts.js
@@ -5,9 +5,10 @@ const key = "a27a2b3b6b42f10d157817f89e2fab38"
 function colocarDadosNaTela(dados) {
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
 async function buscarCidade(cidade) {
@@ -21,4 +22,3 @@ function cliqueiNoBotao() {
 
     buscarCidade(cidade)
 }
-
